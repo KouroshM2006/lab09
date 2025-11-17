46a7ca2d6ad5950b88ea0584aa525904534d88ed
Kourosh Maissamyazad <2440557@edu.vaniercollege.qc.ca>
2025-11-17 10:23:40 -0500

    final commit

--- src/lab09/MainViewController.java
+++ src/lab09/MainViewController.java
@@ -2,9 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-
 //github link: https://github.com/KouroshM2006/lab09.git
-
 package lab09;
 
 import java.net.URL;
@@ -13,10 +11,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.RadioMenuItem;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.BorderPane;
 
 /**
  * FXML Controller class
@@ -49,6 +51,8 @@ public class MainViewController implements Initializable {
     private RadioMenuItem mediumItem;
     @FXML
     private RadioMenuItem largeItem;
+    @FXML
+    private BorderPane mainPane;
 
     /**
      * Initializes the controller class.
@@ -60,50 +64,85 @@ public class MainViewController implements Initializable {
         mediumItem.setToggleGroup(tg);
         largeItem.setToggleGroup(tg);
         closeItem.setDisable(true); // disabled until a file is "opened"
-    }    
+        newItem.setAccelerator(KeyCombination.keyCombination("Ctrl+N"));
+
+        ContextMenu popup = new ContextMenu();
+        MenuItem help = new MenuItem("Help");
+        MenuItem inspect = new MenuItem("Inspect");
+        popup.getItems().addAll(help, inspect);
+        mainPane.setOnMouseClicked(e -> {
+            if (e.getButton() == MouseButton.SECONDARY) {
+                popup.show(mainPane, e.getScreenX() - 50, e.getScreenY()
+                        - 25);
+            }
+        });
+    }
 
     @FXML
     private void handleNew(ActionEvent event) {
+        System.out.println("NEW has been pressed");
+        newItem.setDisable(true);
+        openItem.setDisable(true);
+        closeItem.setDisable(false);
     }
 
     @FXML
     private void handleOpen(ActionEvent event) {
+        System.out.println("OPEN has been pressed");
+        
     }
 
     @FXML
     private void handleClose(ActionEvent event) {
+        System.out.println("CLOSED has been pressed");
+        newItem.setDisable(false);
+        openItem.setDisable(false);
+        closeItem.setDisable(true);
     }
 
     @FXML
     private void handleSave(ActionEvent event) {
+        System.out.println("SAVED has been pressed");
     }
 
     @FXML
     private void handleGrid(ActionEvent event) {
+        if (gridItem.isSelected()) {
+            System.out.println("USE GRIDLINES selected");
+        } else {
+            System.out.println("USE GRIDLINES unselected");
+        }
+
     }
 
     @FXML
     private void handleCopy(ActionEvent event) {
+        System.out.println("COPY has been pressed");
     }
 
     @FXML
     private void handleFind(ActionEvent event) {
+        System.out.println("FIND has been pressed");
     }
 
     @FXML
     private void handleReplace(ActionEvent event) {
+        System.out.println("REPLACE has been pressed");
     }
 
     @FXML
     private void handleSizeSmall(ActionEvent event) {
+        System.out.println("SMALL size chosen");
     }
 
     @FXML
     private void handleSizeMedium(ActionEvent event) {
+        System.out.println("MEDIUM size chosen");
     }
 
     @FXML
     private void handleSizeLarge(ActionEvent event) {
+        System.out.println("LARGE size chosen");
     }
-    
+
 }
